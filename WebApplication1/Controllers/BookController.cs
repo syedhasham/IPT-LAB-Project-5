62045083945bed48eaa927299c13778704f9d637
Syed Hasham <syedhasham333@gmail.com>
2021-01-15 23:54:08 +0500

    completed

---
+++ WebApplication1/Controllers/BookController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class BookController : Controller
+    {
+        Entities context;
+        public BookController()
+        {
+            context = new Entities();
+        }
+        // GET: Book
+        public ActionResult Index()
+        {
+            var books = context.tbl_books; 
+            
+            return View(books);
+        }
+
+        // GET: Book/Details/5
+        public ActionResult Details(int id)
+        {
+            var book = context.tbl_books.Where(b => b.book_id == id).FirstOrDefault();
+            return View(book);
+        }
+
+        // GET: Book/Create
+        public ActionResult Create()
+        {
+           var li = context.tbl_user.ToList();
+            ViewBag.UsersList = new SelectList(li, "user_id", "user_name");
+            return View();
+        }
+
+        // POST: Book/Create
+        [HttpPost]
+        public ActionResult Create(tbl_books collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                var li = context.tbl_user.ToList();
+                ViewBag.UsersList = new SelectList(li, "user_id", "user_name");
+                tbl_books book = new tbl_books
+                {
+                    book_author = collection.book_author,
+                    book_edition = collection.book_edition,
+                    book_name = collection.book_name,
+                    user_id = collection.user_id,
+                    book_price = collection.book_price,
+                    book_qunatity = collection.book_qunatity
+                };
+                context.tbl_books.Add(book);
+                
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch(Exception ex)
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var book = context.tbl_books.Where(b => b.book_id == id).FirstOrDefault();
+
+            return View(book);
+        }
+
+        // POST: Book/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, tbl_books bookModel)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                var book = context.tbl_books.Where(b => b.book_id == id).FirstOrDefault();
+                book.book_author = bookModel.book_author;
+                book.book_edition = bookModel.book_edition;
+                book.book_name = bookModel.book_name;
+                book.book_price = bookModel.book_price;
+                book.book_qunatity = bookModel.book_qunatity;
+                book.user_id = bookModel.user_id;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var book = context.tbl_books.Where(b => b.book_id == id).FirstOrDefault();
+            context.SaveChanges();
+            return View();
+        }
+
+        // POST: Book/Delete/5
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        Entities context;
+        public UserController()
+        {
+            context = new Entities();
+        }
+                // GET: User
+        public ActionResult Index()
+        {
+            var users = context.tbl_user;
+            return View(users);
+        }
+
+        // GET: User/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        public ActionResult Create(tbl_user user)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                context.tbl_user.Add(user);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
